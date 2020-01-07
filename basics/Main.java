04541403e525e80edb2a458adf197aac692e1ab1
Chase Tucker <tuck8106@gmail.com>
2020-01-07 10:48:37 -0800

    pluralize and flipNHeads complete

---
+++ basics/Main.java
@@ -0,0 +1,69 @@
+public class Main {
+  public static void main(String[] args) {
+    int dogCount = 1;
+    System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
+
+    int catCount = 2;
+    System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".");
+
+    int turtleCount = 0;
+    System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
+
+    int heads = 3;
+    int headsCount = flipNHeads(heads);
+    System.out.println("It took " + headsCount + " flips to flip " + heads + pluralize(" head", heads) + " in a row.");
+
+    heads = 0;
+    headsCount = flipNHeads(heads);
+    System.out.println("It took " + headsCount + " flips to flip " + heads + pluralize(" head", heads) + " in a row.");
+
+    heads = 10;
+    headsCount = flipNHeads(heads);
+    System.out.println("It took " + headsCount + " flips to flip " + heads + pluralize(" head", heads) + " in a row.");
+
+    heads = -1;
+    headsCount = flipNHeads(heads);
+    System.out.println("It took " + headsCount + " flips to flip " + heads + pluralize(" head", heads) + " in a row.");
+  }
+
+  /********************************************************** 
+  ** Method Name: pluralize
+  ** Description: method used to pluralize a string if the 
+  ** quantity is greater than 1.
+  ** Input: String to be pluralized, int to determine if the
+  ** string should be pluralized.
+  ** Output: String
+  **********************************************************/
+  public static String pluralize(String str, int count) {
+    if(count != 1) str = str + "s";
+    return str;
+  }
+
+  /********************************************************** 
+  ** Method Name: flipNHeads
+  ** Description: method used to randomly determine the 
+  ** number of times it takes to flip a coin and get a 
+  ** certain number of heads in a row.
+  ** Input: int indicating the number of desired consecutive
+  ** heads
+  ** Output: String with the result of each flip. Returns int 
+  ** indicating the number of flips to get desired number of 
+  ** consecutive flips.
+  **********************************************************/
+  public static int flipNHeads(int n) {
+    int flipCount = 0;
+    int headsCount = 0;
+    while (n > 0 && headsCount < n) {
+      if (Math.random() > 0.5) {
+        System.out.println("heads");
+        headsCount++;
+      }
+      else {
+        System.out.println("tails");
+        headsCount = 0;
+      }
+      flipCount++;
+    }
+    return flipCount;
+  }
+}
